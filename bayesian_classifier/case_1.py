f24f7b5c69dd89fcc384329f5267be247e8608b0
Swapnil Sharma <swap.sha96@gmail.com>
2017-09-14 20:19:49 +0530

    new changes

--- bayesian_classifier/case_1.py
+++ bayesian_classifier/case_1.py
@@ -25,11 +25,11 @@ def calculate_covariance_matrix(data, means):
     for row in data:
         cov[0][0] += ((row[0] - means[0]) ** 2)
         cov[1][1] += ((row[1] - means[1]) ** 2)
-        cov[0][1] += (row[0] - means[0]) * (row[1] - means[1])
+        # cov[0][1] += (row[0] - means[0]) * (row[1] - means[1])
     cov[0][0] /= len(data)
     cov[1][1] /= len(data)
-    cov[0][1] /= len(data)
-    cov[1][0] = cov[0][1]
+    # cov[0][1] /= len(data)
+    # cov[1][0] = cov[0][1]
     return cov
 
 def modulus(matrix_2d):
@@ -55,10 +55,11 @@ def add_matrices(m1, m2):
     return [[m1[0][0] + m2[0][0], m1[0][1] + m2[0][1]], [m1[1][0] + m2[1][0], m1[1][1] + m2[1][1]]]
 
 def divide_matrix_by_x(m, x):
+    matrix = [[0 for x in range(len(m[0]))] for y in range(len(m))]
     for i in range(len(m)):
         for j in range(len(m[0])):
-            m[i][j] = m[i][j]/x
-    return m
+            matrix[i][j] = m[i][j]/x
+    return matrix
 
 def plot_dataset(dataset, color):
     x, y = [], []
@@ -67,61 +68,35 @@ def plot_dataset(dataset, color):
         y.append(row[1])
     plt.scatter(x, y, s= 2, marker ='o', c=color, alpha=1)
 
-def calculate_slope(m1, m2):
-    m = (m2[1] - m1[1])/(m2[0] - m1[0])
-    return -1* (1/m)
+def calculate_sigma_sq(cov):
+    avg_sigma = []
+    for matrix in cov:
+        avg_sigma.append((matrix[0][0] + matrix[1][1]) / 2)
+
+    return (sum(avg_sigma) / len(avg_sigma))
 
 def calculate_w(m1, m2, var, p1, p2):
-    w1 = divide_matrix_by_x(transpose([m1]), var)
-    w2 = divide_matrix_by_x(transpose([m2]), var)
+    w1 = divide_matrix_by_x(m1, var)
+    w2 = divide_matrix_by_x(m2, var)
 
-    pdt1 = multiply_matrices([m1], transpose([m1]))
-    pdt2 = multiply_matrices([m2], transpose([m2]))
-    w10 = math.log(p1) - (pdt1[0][0]/(2*var))
-    w20 = math.log(p2) - (pdt2[0][0]/(2*var))
+    w10 = math.log(p1) - (multiply_matrices(transpose(m1), m1)[0][0] / (2 * var))
+    w20 = math.log(p2) - (multiply_matrices(transpose(m2), m2)[0][0] / (2 * var))
 
     return w1, w2, w10, w20
 
-def gx(w1, w2, w10, w20, x):
-    g1x = multiply_matrices(transpose(w1), x)[0][0] + w10
-    g2x = multiply_matrices(transpose(w2), x)[0][0] + w20
-    return (g1x -g2x)
-
-def get_graph_boundary(dataset1, dataset2):
-    param1 = [[0, 0], [0, 0]]
-    for i, col in zip(range(len(dataset1[0])), zip(*dataset1, *dataset2)):
-        param1[i][0] = min(col)
-        param1[i][1] = max(col)
-    return param1[0][0], param1[0][1], param1[1][0], param1[1][1]
-
-def plot_line(m, x0):
-    x_0, y_0 = x0[0], x0[1]
-    c = y_0 - m*x_0
-
-    x = [x_0]
-    y = [y_0]
-
-    for i in range(20):
-        x.append(i)
-        y.append(m*i+c)
-
-    plt.plot(x, y, 'k')
-
-if __name__ == '__main__':
-    ratio = 0.75
-    names = ['Class1.txt', 'Class2.txt', 'Class3.txt']
-    all_training_sets = []
-    all_means = []
+def generate_summary_from_classes(filenames):
+    all_datasets, all_training_sets, all_test_sets, all_means, all_covariance_matrices, all_probabilities = [], [], [], [], [], []
     sigma_sq = float(0)
-    all_covariance_matrices = []
-    total_data_size = 0
-    all_probabilities = []
+    total_data_size = int(0)
 
-
-    for filename in names:
+    for filename in filenames:
         dataset = load_dataset_from_file(filename)
+        all_datasets.append(dataset)
+
         training_set, test_set = split_dataset(dataset, ratio)
-        all_training_sets.append(training_set) # store t set
+        all_training_sets.append(training_set)
+        all_test_sets.append(test_set)
+
         means = mean(training_set)
         all_means.append(means)
 
@@ -134,18 +109,28 @@ if __name__ == '__main__':
     for i in range(len(all_probabilities)):
         all_probabilities[i] /= total_data_size
 
+    return all_datasets, all_training_sets, all_test_sets, all_means, all_covariance_matrices, all_probabilities
 
-    avg_covariance_matrix = divide_matrix_by_x(add_matrices(add_matrices(all_covariance_matrices[0], all_covariance_matrices[1]), all_covariance_matrices[2]), 3)
-    sigma_sq = modulus(avg_covariance_matrix)
-    w1, w2, w10, w20 = calculate_w(all_means[0], all_means[1], sigma_sq, all_probabilities[0], all_probabilities[1])
+def gx(w1, w2, w10, w20, x):
+    g1x = multiply_matrices(transpose(w1), x)[0][0] + w10
+    g2x = multiply_matrices(transpose(w2), x)[0][0] + w20
 
-    x_min, x_max, y_min, y_max = get_graph_boundary(all_training_sets[0], all_training_sets[1])
+    return (g1x - g2x)
 
-    num_of_points = 500
+def get_graph_boundary(dataset):
+    param = [[0, 0], [0, 0]]
+    for i, col in zip(range(len(dataset[0])), zip(*dataset)):
+        param[i][0], param[i][1] = min(col), max(col)
+
+    x_min, x_max, y_min, y_max = param[0][0], param[0][1], param[1][0], param[1][1]
     x_min -= 1
     x_max += 1
     y_min -= 1
     y_max += 1
+    return x_min, x_max, y_min, y_max
+
+def plot_decision_boundary(x_min, x_max, y_min, y_max):
+    num_of_points = 500
     x_diff = (x_max - x_min) / num_of_points
     y_diff = (y_max - y_min) / num_of_points
 
@@ -161,15 +146,12 @@ if __name__ == '__main__':
             y.append(y_i)
 
     count = 0
-    cls1_x = []
-    cls1_y = []
-    cls2_x = []
-    cls2_y = []
+    cls1_x, cls1_y, cls2_x, cls2_y = [], [], [], []
+
     for x_i, y_i in zip(x, y):
         count += 1
         x_matrix = [[x_i], [y_i]]
         fx = gx(w1, w2, w10, w20, x_matrix)
-        print(count, fx)
         if fx >= 0:
             cls1_x.append(x_i)
             cls1_y.append(y_i)
@@ -179,16 +161,24 @@ if __name__ == '__main__':
 
     plt.scatter(cls1_x, cls1_y, s= 3, marker ='o', c='r', alpha=1)
     plt.scatter(cls2_x, cls2_y, s= 3, marker ='o', c='b', alpha=1)
-    plot_dataset(all_training_sets[0], 'g')
-    plot_dataset(all_training_sets[1], 'g')
 
-    # plot_dataset(all_training_sets[0], 'r')
-    # plot_dataset(all_training_sets[1], 'b')
+if __name__ == '__main__':
+    ratio = 0.75
+    dirname = './sample/'
+    names = [dirname + 'Class1.txt', dirname + 'Class2.txt']
+    
+    all_datasets, all_training_sets, all_test_sets, all_means, all_covariance_matrices, all_probabilities = generate_summary_from_classes(names)
 
-    # x0 = calculate_x0(all_means[1], all_means[2], sigma_sq, all_probabilities[1], all_probabilities[2])
-    # plot_line(slope, x0)
+    sigma_sq = calculate_sigma_sq(all_covariance_matrices)
+    w1, w2, w10, w20 = calculate_w(transpose([all_means[0]]), transpose([all_means[1]]), sigma_sq, all_probabilities[0], all_probabilities[1])
 
-    # x0 = calculate_x0(all_means[2], all_means[0], sigma_sq, all_probabilities[2], all_probabilities[0])
-    # plot_line(slope, x0)
+    x_min, x_max, y_min, y_max = get_graph_boundary(all_datasets[0] + all_datasets[1])
+
+    plot_decision_boundary(x_min, x_max, y_min, y_max)
+
+    plot_dataset(all_training_sets[0] + all_training_sets[1], 'g')
+    plot_dataset(all_test_sets[0], 'k')
+    plot_dataset(all_test_sets[1], 'w')
+
 
     plt.show()
